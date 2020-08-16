1efc919bdc614f05c19df8c58e8f887dc0bdd340
Stephen Drodge <stephen.drodge@gmail.com>
2020-08-16 09:38:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rusty Fireworks
+A web server for playing [Hanabi](https://boardgamegeek.com/boardgame/98778/hanabi) online.
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::any().map(|| "Hello, World!");
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
